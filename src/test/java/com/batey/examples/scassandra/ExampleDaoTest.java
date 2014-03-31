22988d059ae0ddf98c221142c0e9115157fe9443
Christopher Batey <christopher.batey@gmail.com>
2014-03-31 17:20:00 -0600

    Moving java client out into a separate project

--- src/test/java/com/batey/examples/scassandra/ExampleDaoTest.java
+++ src/test/java/com/batey/examples/scassandra/ExampleDaoTest.java
@@ -1,24 +1,18 @@
 package com.batey.examples.scassandra;
 
-import com.google.gson.Gson;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.scassandra.http.client.PrimingClient;
+import org.scassandra.http.client.PrimingRequest;
 import uk.co.scassandra.ServerStubRunner;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ExampleDaoTest {
 
@@ -44,53 +38,21 @@ public class ExampleDaoTest {
         serverStub.shutdown();
     }
 
-
     @Test
-    public void test() throws Exception{
+    public void testRetrievingOfNames() throws Exception{
+        // given
         Map<String, String> row = new HashMap<>();
-        row.put("name", "Chris");
+        String primedName = "Chris";
+        row.put("name", primedName);
         primingClient.prime(new PrimingRequest("select * from people", Arrays.asList(row)));
-
         ExampleDao exampleDao = new ExampleDao(portNumber);
+
+        //when
         exampleDao.connect();
         List<String> names = exampleDao.retrieveNames();
+        
+        //then
         assertEquals(1, names.size());
-    }
-
-    private static class PrimingClient {
-        private String host;
-        private int port;
-        private Gson gson = new Gson();
-
-        public PrimingClient(String host, int port) {
-            this.host = host;
-            this.port = port;
-        }
-
-        public void prime(PrimingRequest primeRequest) throws PrimeFailedException {
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            HttpPost httpPost = new HttpPost("http://"+host+":" + port + "/prime");
-            String jsonAsString = gson.toJson(primeRequest);
-            httpPost.setEntity(new StringEntity(jsonAsString, ContentType.APPLICATION_JSON));
-            try {
-                CloseableHttpResponse response1 = httpClient.execute(httpPost);
-                if (response1.getStatusLine().getStatusCode() != 200) throw new PrimeFailedException();
-            } catch (IOException e) {
-                throw new PrimeFailedException();
-            }
-
-        }
-    }
-
-    private static class PrimeFailedException extends RuntimeException {}
-
-    private static class PrimingRequest {
-        private String when;
-        private List<Map<String, String>> then;
-
-        private PrimingRequest(String when, List<Map<String, String>> rows) {
-            this.when = when;
-            this.then = rows;
-        }
+        assertEquals(primedName, names.get(0));
     }
 }
