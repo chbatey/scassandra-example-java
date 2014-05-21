22ec4c49ab001267202ebd6c78f20f71c9cd40e1
Christopher Batey <christopher.batey@gmail.com>
2014-05-21 18:45:47 +0100

    adding copywright

--- copyright.groovy
+++ copyright.groovy
@@ -1,4 +1,4 @@
-/*
+def copyrightNotice = """/*
  * Copyright (C) 2014 Christopher Batey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.batey.examples.scassandra;
+"""
 
-public class UnableToRetrievePeopleException extends RuntimeException {
-}
+    def srcDir = new File('src')
+    srcDir.eachFileRecurse { file ->
+        if (file.name.endsWith(".java") && !file.text.contains(copyrightNotice)) {
+            println "Adding copyright header to $file.path"
+            def newFileText = copyrightNotice + file.text;
+            file.text = newFileText;
+        }
+    }
